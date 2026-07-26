826b124d317d0765fd0e56de41af86c097fbe8d8
Namrata Rawat <namratarwt96@gmail.com>
2026-07-26 12:42:21 +0530

    Update script.js

--- task3-clone-ui-of-app/script.js
+++ task3-clone-ui-of-app/script.js
@@ -1,1 +1,27 @@
 
+// Sidebar Toggle
+
+const menu = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener("click", () => {
+    sidebar.classList.toggle("hide");
+});
+
+// Category Active Button
+
+const buttons = document.querySelectorAll(".categories button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        buttons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+    });
+
+});
